d757421e6f9eb950dd16f68765f41be8973882fa
Alex <alex.a.mylonas@gmail.com>
2020-04-21 22:15:53 +0300

    GlobalModuleDetails Component

---
+++ web/src/pages/GlobalModuleDetails/GlobalModuleDetails.tsx
@@ -0,0 +1,69 @@
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import useRouter from 'Hooks/useRouter';
+import { Alert } from 'pouncejs';
+import { extractErrorMessage } from 'Helpers/utils';
+import ErrorBoundary from 'Components/ErrorBoundary';
+import GlobalModuleDetailsInfo from './GlobalModuleDetailsInfo';
+import GlobalModuleDetailsPageSkeleton from './Skeleton';
+import { useGlobalModuleDetails } from './graphql/globalModuleDetails.generated';
+
+const GlobalModuleDetailsPage = () => {
+  const { match } = useRouter<{ id: string }>();
+
+  console.log('match', match);
+  const { error, data, loading } = useGlobalModuleDetails({
+    fetchPolicy: 'cache-and-network',
+    variables: {
+      globalModuleDetailsInput: {
+        globalId: match.params.id,
+      },
+    },
+  });
+
+  if (loading && !data) {
+    return <GlobalModuleDetailsPageSkeleton />;
+  }
+
+  console.log('GLOBAL MODULE', data);
+  if (error) {
+    return (
+      <Alert
+        variant="error"
+        title="Couldn't load global module"
+        description={
+          extractErrorMessage(error) ||
+          "An unknown error occurred and we couldn't load the global module from the server"
+        }
+        mb={6}
+      />
+    );
+  }
+
+  return (
+    <article>
+      <ErrorBoundary>
+        <GlobalModuleDetailsInfo global={data.globalModule} />
+      </ErrorBoundary>
+    </article>
+  );
+};
+
+export default GlobalModuleDetailsPage;
---
+++ web/src/pages/GlobalModuleDetails/GlobalModuleDetailsInfo/GlobalModuleDetailsInfo.tsx
@@ -0,0 +1,87 @@
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import { Box, Grid, Label, Text } from 'pouncejs';
+import { formatDatetime } from 'Helpers/utils';
+import Panel from 'Components/Panel';
+import Linkify from 'Components/Linkify';
+import { GlobalModuleDetails } from 'Generated/schema';
+
+interface GlobalModuleDetailsInfoProps {
+  global?: GlobalModuleDetails;
+}
+
+const GlobalModuleDetailsInfo: React.FC<GlobalModuleDetailsInfoProps> = ({ global }) => {
+  return (
+    <Panel
+      size="large"
+      title="Global Module Details"
+      // actions={
+      //   <Box>
+      //     <Button
+      //       size="large"
+      //       variant="default"
+      //       mr={4}
+      //       is={Link}
+      //       to={urls.compliance.policies.edit(policy.id)}
+      //     >
+      //       Edit
+      //     </Button>
+      //   </Box>
+      // }
+    >
+      <Grid gridTemplateColumns="repeat(2, 1fr)" gridGap={6}>
+        <Box my={1}>
+          <Label mb={1} is="div" size="small" color="grey300">
+            ID
+          </Label>
+          <Text size="medium" color="black">
+            {global.id}
+          </Text>
+        </Box>
+        <Box my={1}>
+          <Label mb={1} is="div" size="small" color="grey300">
+            DESCRIPTION
+          </Label>
+          <Text size="medium" color={global.description ? 'black' : 'grey200'}>
+            <Linkify>{global.description || 'No description available'}</Linkify>
+          </Text>
+        </Box>
+        <Box my={1}>
+          <Label mb={1} is="div" size="small" color="grey300">
+            CREATED
+          </Label>
+          <Text size="medium" color="black">
+            {formatDatetime(global.createdAt)}
+          </Text>
+        </Box>
+        <Box my={1}>
+          <Label mb={1} is="div" size="small" color="grey300">
+            LAST MODIFIED
+          </Label>
+          <Text size="medium" color="black">
+            {formatDatetime(global.lastModified)}
+          </Text>
+        </Box>
+      </Grid>
+    </Panel>
+  );
+};
+
+export default React.memo(GlobalModuleDetailsInfo);

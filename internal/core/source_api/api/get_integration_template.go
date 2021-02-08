f3f3492aa448e796e65ce088af654000d60ff02b
Russell Leighton <russell.leighton@runpanther.io>
2021-02-08 15:31:59 -0500

    Improve template error message (#2626)
    * Update s3 paths for log subscriptions for cloud_security and rule_errors
    
    * Make error message for template failures more useful to help debugging
    
    * mage gen fmt
    
    * Make error message nicer
    
    Co-authored-by: panther-bot <github-service-account@runpanther.io>

--- internal/core/source_api/api/get_integration_template.go
+++ internal/core/source_api/api/get_integration_template.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 
 	"github.com/panther-labs/panther/api/lambda/source/models"
@@ -130,7 +131,8 @@ func (api *API) getTemplate(integrationType string) (string, error) {
 	zap.L().Debug("requesting template", zap.String("key", *templateRequest.Key), zap.String("bucket", *templateRequest.Bucket))
 	s3Object, err := api.TemplateS3Client.GetObject(templateRequest)
 	if err != nil {
-		return "", err
+		return "", errors.Wrapf(err, "cannot read template s3://%s/%s, check Panther VERSION file",
+			*templateRequest.Bucket, *templateRequest.Key)
 	}
 
 	// Load the s3Object into memory. They're only ~8Kb in size.
